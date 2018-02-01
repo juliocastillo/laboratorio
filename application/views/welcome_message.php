87816f1418197059c6b19678edc4612d89aa71cc
Monica Castillo <monicastilloh9@gmail.com>
2018-01-31 20:09:51 -0600

    Mejorando la pantalla de bienvenida

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -71,7 +71,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 	<h1>Welcome to CodeIgniter!</h1>
 
 	<div id="body">
-		Sistema de Laboratorio
+		Sistema de Laboratorio clinico
 	</div>
 
 	<p class="footer">Page rendered in <strong>{elapsed_time}</strong> seconds. <?php echo  (ENVIRONMENT === 'development') ?  'CodeIgniter Version <strong>' . CI_VERSION . '</strong>' : '' ?></p>
