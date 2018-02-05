aa8e153e9bf583ba85075006f916769ae7ac44bb
Monica Castillo <monicastilloh9@gmail.com>
2018-02-05 17:26:58 -0600

    Agregar botones de procesos, reportes, listas y configuracion

--- application/views/menu.php
+++ application/views/menu.php
@@ -10,5 +10,40 @@
 	    <div class="dropdown-divider"></div>
 	    <button class="dropdown-item" type="button">Something else here</button>
 	  </div>
+	
+	  <button class="btn btn-success dropdown-toggle" type="button" id="dropdownMenu2" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+	   <i class="fa fa-list-alt"></i>
+	   Reportes
+	  </button>
+	  <div class="dropdown-menu" aria-labelledby="dropdownMenu2">
+	    <button class="dropdown-item" type="button">Crear solicitud</button>
+	    <button class="dropdown-item" type="button">Another action</button>
+	    <div class="dropdown-divider"></div>
+	    <button class="dropdown-item" type="button">Something else here</button>
+	  </div>
+
+	  <button class="btn btn-warning dropdown-toggle" type="button" id="dropdownMenu2" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+	   <i class="fa fa-tasks"></i>
+	   Listas
+	  </button>
+	  <div class="dropdown-menu" aria-labelledby="dropdownMenu2">
+	    <button class="dropdown-item" type="button">Crear solicitud</button>
+	    <button class="dropdown-item" type="button">Another action</button>
+	    <div class="dropdown-divider"></div>
+	    <button class="dropdown-item" type="button">Something else here</button>
+	  </div>
+
+
+	  <button class="btn btn-danger dropdown-toggle" type="button" id="dropdownMenu2" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+	   <i class="fa fa-cogs"></i>
+	   Configuracion
+	  </button>
+	  <div class="dropdown-menu" aria-labelledby="dropdownMenu2">
+	    <button class="dropdown-item" type="button">Crear solicitud</button>
+	    <button class="dropdown-item" type="button">Another action</button>
+	    <div class="dropdown-divider"></div>
+	    <button class="dropdown-item" type="button">Something else here</button>
+	  </div>
+
 	</div>
 </div>
